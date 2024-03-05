5206b2e5cc99ae78eea55bb42558655277688a68
Nicholas Malm <nickemalm00@gmail.com>
2024-03-05 13:49:29 +0100

    added rules.py with method to print rules

---
+++ rules.py
@@ -0,0 +1,24 @@
+import tools
+
+
+class Rules:
+    def __init__(self):
+        self.tools = tools.Tools()
+
+    def show_rules(self):
+        print('''Welcome to the PIG Dice Game!
+        Here are the rules:
+        1. The game is played by two players.
+           A human player and a computer player.
+        2. The game is turn-based. The human player goes first.
+        3. The goal of the game is to reach 100 points.
+        4. If a player rolls a 1, the player's turn ends and the player
+           loses all points accumulated during the turn.
+        5. If a player rolls a 2-6, the player can choose to roll again.
+        6. If a player chooses to hold, the points accumulated during the
+           turn are added to the player's total points.
+        7. The computer player is set to easy difficulty by default.
+        ''')
+        input('Press enter to continue...')
+        self.tools.clear_screen()
+        return
