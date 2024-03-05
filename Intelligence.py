cc0632448edcdea5f1d9c060b149ab738812d61e
Nicholas Malm <nickemalm00@gmail.com>
2024-03-05 11:08:48 +0100

    fixing errors

--- Intelligence.py
+++ Intelligence.py
@@ -1,4 +1,36 @@
+import dice
+# import random
+
+
 class Intelligence:
 
-    def __init__(self):
+    def init(self):
+        self.intelligence_name = "Computer"
+        self.difficulty = 0
+        self.total_score = 0
+        self.round_score = 0
+        self.die = dice.Dice()
+
+    def start_round(difficulty):
+        if difficulty == 1:
+            pass
+
+        elif difficulty == 2:
+            print("Medium")
+
+        elif difficulty == 3:
+            print("Hard")
+
+    def hold(self):
+        self.total_score += self.round_score
+        self.round_score == 0
+        return
+
+    def easy():
+        pass
+
+    def medium():
+        pass
+
+    def hard():
         pass
