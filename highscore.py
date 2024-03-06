9e30b96bcb2623a4317b3deefc01e2c3efa89616
jacob hellgren <hellgrenjacob@hotmail.com>
2024-03-06 21:54:05 +0100

    Updates to highscore, new logic

--- highscore.py
+++ highscore.py
@@ -1,19 +1,21 @@
+import player
+
 class Highscore():
 
     def __init__(self):
-        self.player_highscore = []
+        self.player_highscore = {}
 
     def add_highscore(self, score):
         self.player_highscore.append(score)
         self.player_highscore.sort(reverse=True)
 
-    def print_highscore(self):
-        if self.player_highscore:
-            print(f'''Your current Highscore is {self.player_highscore}
-                  in one round! ''')
-        else:
-            print('No current Highscores. Time to play!')
+    def save_player_highscore(self, filename):
+        with open(filename, "a") as highscore_file:
+            for player, highscore in self.player_highscore.items():
+                highscore_file.write(f"{player}: {highscore}\n")
 
-    def print_top_scores(self, top=5):
-        for i in range(0, top):
-            print(f'{i+1}. {self.player_highscore[i]}')
+    def print_highscore(self, filename):
+        with open(filename, "r") as open_highscore_file:
+            for line in open_highscore_file:
+                player, 
+
