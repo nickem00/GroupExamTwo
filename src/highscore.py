595bb6326a048f2edfa421e40c5968847e115c99
Nicholas Malm <nickemalm00@gmail.com>
2024-03-07 23:50:27 +0100

    modified the Highscore.py file

--- src/highscore.py
+++ src/highscore.py
@@ -1,66 +1,96 @@
-import player
+import tools
 
 
 class Highscore():
-
     def __init__(self):
-        self.highscore_file = "highscore.txt"
+        self.highscoreFile = 'highscore.txt'
+        self.tools = tools.Tools()
 
-    def new_highscore(self, player_name, player_score, filename):
-        highscores = self.highscore_tracker(filename)
+    '''
+    A method for reading the highscores from the highscore file.
+    It reads the highscores from the highscore file and returns
+    a dictionary with the highscores for use in other functions.
+    '''
+    def read_highscores(self):
+        try:
+            with open(self.highscoreFile, "r") as highscore_file:
+                highscores = {}
+                for line in highscore_file:
+                    player, highscore = line.rstrip("\n").split(":")
+                    highscores[player] = int(highscore)
+                return highscores
+        except FileNotFoundError:
+            return {}
 
-        if not highscores:
-            self.save_new_highscore(player_name, player_score, filename)
-        
+    '''
+    A method for displaying the highscores. It uses the read_highscores
+    method to read the highscores from the highscore file, and then
+    prints the highscores.
+    '''
+    def display_highscores(self):
+        self.tools.clear_screen()
+        highscores = self.read_highscores()
+        if highscores:
+            print("Highscores:")
+            print("-----------\n")
+            i = 0
+            for player, score in list(highscores.items())[:5]:
+                i += 1
+                print(f"{i}. {player}: {score}")
+            print()
         else:
-            current_highscoreholder = list(highscores.keys())[-1]
-            current_highscoreholder_score = highscores[current_highscoreholder]
+            print("No highscores found.")
+        self.tools.enter_to_continue()
 
-            if player_score > current_highscoreholder_score:
-                self.save_new_highscore(player_name, player_score, filename)
+    '''
+    A method for checking if a player has a highscore. Not sure if
+    this method is needed, but it is here for now. It takes in the
+    player name and score, and checks if the player already has a
+    highscore. If the player has a highscore, the method will return
+    True if the new score is higher than the old one, else False. If the player
+    does not have a highscore, the method will return True.
+    '''
+    def is_highscore(self, player_name, player_score):
+        highscores = self.read_highscores()
+        if player_name in highscores:
+            if player_score > highscores[player_name]:
+                return True
+            else:
+                return False
+        else:
+            return True
+
+    '''
+    A method for saving a new highscore. It takes in the player name and score,
+    checks if the player already has a highscore, and if so, if the
+    new score is higher than the old one. If the player has a highscore,
+    the method will update the highscore if the new score is higher.
+    If the player does not have a highscore, the method will add
+    the player and score to the highscore list.
+
+    The method will then sort the highscores and write them to the
+    highscore file.
+    '''
+    def save_new_highscore(self, player_name, player_score):
+        highscores = self.read_highscores()
+        if player_name in highscores:
+            if player_score > highscores[player_name]:
+                highscores[player_name] = player_score
+                print(f'New highscore! ({player_score}) Your highest '
+                      'score is now saved!')
+                self.tools.enter_to_continue()
             else:
                 pass
-
-    def highscore_tracker(self, highscore_file):
-            try:
-                with open(highscore_file, "r") as highscore_file:
-                    highscore_dictionary = {}
-                    for line in highscore_file:
-                        player, highscore = line.rstrip("\n").split(":")
-                        highscore_dictionary[player] = int(highscore)
-                    return highscore_dictionary
-            except FileNotFoundError:
-                print("Error: File not found")
-                return {}  
-
-    def save_new_highscore(self, player_name, player_score):
-        try:
-            highscores = self.highscore_tracker(self.highscore_file)
+        else:
             highscores[player_name] = player_score
+            print('Congratulations! Your highest score is now saved!')
+            self.tools.enter_to_continue()
 
-            with open(self.highscore_file, "w") as highscore_file:
-                for player, score in highscores.items():
-                    highscore_file.write(f"{player}: {score}\n")
-                
-        except FileNotFoundError:
-            print("Error: File not found")
+        highscore_sorting = highscores.items()
+        sorted_list = sorted(highscore_sorting,
+                             key=lambda item: item[1], reverse=True)
+        sorted_highscores = dict(sorted_list)
 
-
-    #This function displays the top 5 highscores from the highscore_file.
-    def display_highscore(self, highscore_file):
-        with open(highscore_file, "r") as top_scores:
-            highscores = [line.strip() for line in top_scores]
-            highscores_reversed = highscores[::-1]
-
-            if highscores:
-                print("--Highscore leaderboard--")
-                count = 0
-                for items in highscores_reversed:
-                    name, scores = items.split(":")
-                    print(f"{name}: {scores}")
-                    count += 1
-                    if count >= 5:
-                        break            
-            else:
-                print("No highscores have been added")
-            
+        with open(self.highscoreFile, "w") as highscore_file:
+            for player, score in sorted_highscores.items():
+                highscore_file.write(f"{player}: {score}\n")
