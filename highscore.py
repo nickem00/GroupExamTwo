4d105ebffbe916e3c74079067cb679e5ac9768e6
Nicholas Malm <nickemalm00@gmail.com>
2024-03-07 11:58:14 +0100

    Imported Jacbos highscore

--- highscore.py
+++ highscore.py
@@ -1,15 +1,20 @@
 import player
+
+
 class Highscore():
 
-    def __init__(self):
+    def init(self):
         pass
 
     def new_highscore(self, player_name, player_score, filename):
         current_highscore = self.highscore_tracker(filename)
 
+        if player_score > current_highscore:
+            self.save_new_highscore(player_name, player_score, filename)
+        else:
+            pass
 
     def highscore_tracker(self, filename):
-
         with open(filename, "r") as highscore_file:
             highscore_dictionary = {}
             for line in highscore_file:
@@ -18,4 +23,6 @@ class Highscore():
             return highscore_dictionary
 
     def save_new_highscore(self, player_name, player_score, filename):
-         pass
+        with open(filename, "w") as highscore_file:
+            for player, score in highscore_file:
+                highscore_file.write(f"{player} with a score of: {score}")
