1e6959fa3f2ef853ab789208305804ec690383e2
jacob hellgren <hellgrenjacob@hotmail.com>
2024-03-08 13:11:04 +0100

    unittest tools

--- tests/.test_tools.py
+++ tests/.test_tools.py
@@ -6,8 +6,34 @@ class TestTools(unittest.TestCase):
     def test_init_(self):
         self.tools = tools.Tools()
 
-
-    @patch("tests")
-    def test_clear_screen(self, mock_os):
+    @patch('os.name')
+    def test_clear_screen_windows(self, mock_os_name):
+        mock_os.return_value = 'nt'
         self.tools.clear_screen()
-        mock_os.system.assert_called_with("clear")
+        self.tools.clear_screen()
+        mock_os_system.assert_called_with('cls')
+
+    @patch('os.name')
+    def test_clear_screen_unix(self, mock_os_name):
+            mock_os.return_value = 'posix'
+            self.tools.clear_screen()
+            mock_os_system.assert_called_with('clear')
+    
+    @patch("sys.exit")
+    def test_close_game(self, mock_sys_exit):
+         self.tools.close_game()
+         mock_sys_exit.assert_called_with()
+         print_message = self.assertRaises(SystemExit, self.tools.close_game)
+         self.assertEqual(str(print_message.argv[0]), "Thank you for playing!")
+
+    @patch("time.sleep")
+    def test_pause(self, mock_time_sleep):
+         pause_duration = 2
+         self.tools.pause(pause_duration)
+         mock_time_sleep.assert_called_with(pause_duration)
+
+    
+    def test_enter_to_continue(self):
+         with patch("bultin.input", return_value = ""):
+              self.tools.enter_to_continue()
+              self.assertTrue(self.tools.clear_screen.called)
